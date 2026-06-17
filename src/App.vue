052fa1377568fc16e231da5a8fc73fb02a11f84a
Anton Ledyaev <9787@mail.ru>
2026-06-17 18:50:18 +0200

    added basic model and template for a posts list

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,105 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Create a post</h4>
+      <input
+          v-bind:value="title"
+          @input="title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Post Title"
+      >
+      <input
+          v-bind:value="body"
+          @input="body = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Post Body"
+      >
+      <button class="btn" @click="createPost">Create</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Title: </strong> {{ post.title }}</div>
+      <div><strong>Body: </strong> {{ post.body }}</div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+  export default {
+    data() {
+      return {
+        posts: [
+          {
+            id: 1,
+            title: 'Javascript',
+            body: "JS is a programming language",
+          },
+          {
+            id: 2,
+            title: 'CSS',
+            body: "CSS is a style language",
+          },
+          {
+            id: 3,
+            title: 'HTML',
+            body: "HTML is a markup language",
+          }
+        ],
+        title: '',
+        body: '',
+      }
+    },
+    methods: {
+      createPost() {
+        const newPost = {
+          id: Date.now(),
+          title: this.title,
+          body: this.body,
+        }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+        this.posts.push(newPost);
+        this.title = "";
+        this.body = "";
+      },
+    }
+  }
+</script>
 
-@media (min-width: 1024px) {
-  header {
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  .app {
+    padding: 20px;
+  }
+
+  form {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-direction: column;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .post {
+    padding: 20px;
+    border: 2px solid teal;
+    margin-top: 15px;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 20px;
   }
-}
+
+  .btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    border: 1px solid teal;
+  }
 </style>
